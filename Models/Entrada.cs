fdc4bfc519277d08bea6c9762186101b5b0ad515
Ezequiel Feola <ezefeola@gmail.com>
2024-09-10 14:26:58 -0300

    Entrada model con BaseModel

--- Models/Entrada.cs
+++ Models/Entrada.cs
@@ -1,10 +1,12 @@
 ﻿namespace template_csharp_dotnet.Models
 {
-    public class Entrada
+    public class Entrada : BaseModel
     {
-        public int Id { get; set; }
         public string CodigoQR { get; set; }
         public bool Verificada { get; set; }
         public int UsuarioId { get; set; }
+
+        //Navigation Property
+
     }
 }
