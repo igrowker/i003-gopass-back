bb6db2643d3a3ecb023512d88f6b8ec3e1d68cb9
Ezequiel Feola <ezefeola@gmail.com>
2024-09-10 14:13:56 -0300

    ApplicationDbContext y NugetPackages Agregados

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace template_csharp_dotnet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
