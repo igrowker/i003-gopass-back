df0d02707421bff9cb5abed82c1bbff86bdaa39e
Ezequiel Feola <ezefeola@gmail.com>
2024-09-10 14:28:13 -0300

    Usuario Model

---
+++ Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace template_csharp_dotnet.Models
+{
+    public class Usuario : BaseModel
+    {
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public string NumeroTelefono { get; set; }
+        public bool Verificado { get; set; }   
+    }
+}
