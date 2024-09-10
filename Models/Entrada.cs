0f8c1ce4b519e96297f8616d2c4d1cbc2acab66a
Ezequiel Feola <ezefeola@gmail.com>
2024-09-10 14:21:42 -0300

    Entrada Model comun

---
+++ Models/Entrada.cs
@@ -0,0 +1,10 @@
+﻿namespace template_csharp_dotnet.Models
+{
+    public class Entrada
+    {
+        public int Id { get; set; }
+        public string CodigoQR { get; set; }
+        public bool Verificada { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
