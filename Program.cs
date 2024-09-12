f9be8aacefcbdbdce079bb6a77ca5b2f4d688c9f
Nahuel Medina <nahuelmedina2000@gmail.com>
2024-09-12 13:32:37 -0300

    NugetPackage JWT Configuración .Json JWT Servicios y Middlewares JWT

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using template_csharp_dotnet.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +20,27 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
   options.UseSqlServer(connectionString);
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 #endregion Services Area
 
 var app = builder.Build();
@@ -28,8 +52,10 @@ if (app.Environment.IsDevelopment())
   app.UseSwagger();
   app.UseSwaggerUI();
 }
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 
 #endregion Middlewares Area
