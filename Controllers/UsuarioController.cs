965c6a2f391103c3da0608c247d92490b0b2d21f
Ezequiel Feola <ezefeola@gmail.com>
2024-09-12 15:19:04 -0300

    UsuarioController Create y Get Methods

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using template_csharp_dotnet.DTOs.Request;
+using template_csharp_dotnet.Models;
+using template_csharp_dotnet.Services.Interfaces;
+using template_csharp_dotnet.Utilities.Mappers;
+
+namespace template_csharp_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet("Get-Users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            try
+            {
+                var users = await _usuarioService.GetAllUsers();
+
+                return Ok(users);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Create-User")]
+        public async Task<IActionResult> CreateUser(UsuarioRequestDto usuarioRequestDto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var userToCreate = usuarioRequestDto.ToModel();
+                await _usuarioService.CreateUser(userToCreate);
+
+                return Ok(userToCreate.ToResponseDto());
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+    }
+}
