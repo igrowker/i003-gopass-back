f0c3f95560f2d65421529dc315b170b830ef9f43
JL Wariord GP <gomez.jorge.pa@outlook.com>
2024-09-12 09:32:08 -0500

    DTOs y Mappers de entrada

---
+++ DTOs/Request/EntradaRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using template_csharp_dotnet.Models;
+
+namespace template_csharp_dotnet.DTOs.Request
+{
+    public class EntradaRequestDto
+    {
+        public string CodigoQR { get; set; }
+        public bool Verificada { get; set; }
+        public int UsuarioId { get; set; }
+
+        //Navigation Property
+
+        //   public Usuario Usuario { get; set; }
+        //   public Reventa Reventa { get; set; }
+    }
+}
