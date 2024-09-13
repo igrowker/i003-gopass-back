aebe2788d1e4fbe9e8c1d156d8b02301648ad18d
JL Wariord GP <gomez.jorge.pa@outlook.com>
2024-09-13 10:24:53 -0500

    Agregado AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using template_csharp_dotnet.Data;
+
+
+namespace template_csharp_dotnet.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login([FromBody] LoginRequest login)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.DNI == login.DNI && u.Password == login.Password);
+
+            if (usuario == null) return Unauthorized();
+
+            var token = GenerateJwtToken(usuario);
+
+            return Ok(new { Token = token });
+        }
+
+        private string GenerateJwtToken(ApplicationUser usuario)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, usuario.DNI),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("usuarioId", usuario.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LLAVE"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                claims: claims,
+                expires:  DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string DNI { get; set; }
+        public string Password { get; set; }
+    }
+}
