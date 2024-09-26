c287726db9874d2b16f16a9fd2b47138afee0143
Adhe-Enne <ninadaniel_service@hotmail.com>
2024-09-25 22:21:43 -0300

    feat: Se agrega un servicio Dummy para verificacion de entradas a partir de Qr

---
+++ DTOs/Response/TicketResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace template_csharp_dotnet.DTOs.Response
+{
+    public class TicketResponseDto
+    {
+        public string TicketId { get; set; }
+        public bool IsValid { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
