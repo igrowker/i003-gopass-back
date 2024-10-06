5d435df81b4f14494df89b65a0d4d7bd0ceaca68
JL Wariord GP <gomez.jorge.pa@outlook.com>
2024-10-06 12:09:31 -0500

    Feature : Implementacion de restablecimiento de contraseña

---
+++ GoPass.Domain/DTOs/Request/AuthRequestDTOs/PasswordResetRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPass.Domain.DTOs.Request.AuthRequestDTOs
+{
+    public class PasswordResetRequestDto
+    {
+        public string Email { get; set; } = default!;
+    }
+}
