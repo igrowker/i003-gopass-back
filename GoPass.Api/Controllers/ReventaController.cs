749f75b26a11425aa846848efe738a7ef76e2f25
Ezefeola <86271489+Ezefeola@users.noreply.github.com>
2024-10-09 17:12:45 -0300

    Revert "Se agrega Test Unitarios para ReventaController"

--- GoPass.Api/Controllers/ReventaController.cs
+++ GoPass.Api/Controllers/ReventaController.cs
@@ -70,32 +70,25 @@ namespace GoPass.API.Controllers
         [HttpPost("publicar-entrada-reventa")]
         public async Task<IActionResult> PublishResaleTicket(PublishReventaRequestDto publishReventaRequestDto)
         {
-            try
-            {
-                string authorizationHeader = Request.Headers["Authorization"].ToString();
-                string userIdObtainedString = await _usuarioService.GetUserIdByTokenAsync(authorizationHeader);
-                int userId = int.Parse(userIdObtainedString);
+            string authorizationHeader = Request.Headers["Authorization"].ToString();
+            string userIdObtainedString = await _usuarioService.GetUserIdByTokenAsync(authorizationHeader);
+            int userId = int.Parse(userIdObtainedString);
 
-                bool validUserCredentials = await _usuarioService.ValidateUserCredentialsToPublishTicket(userId);
+            bool validUserCredentials = await _usuarioService.ValidateUserCredentialsToPublishTicket(userId);
 
-                if (validUserCredentials == false) return BadRequest("Debe tener todas sus credenciales en regla para poder publicar una entrada");
+            if (validUserCredentials == false) return BadRequest("Debe tener todas sus credenciales en regla para poder publicar una entrada");
 
-                Entrada verifiedTicket = await _gopassHttpClientService.GetTicketByQrAsync(publishReventaRequestDto.CodigoQR);
-                PublishEntradaRequestDto existingTicketInFaker = verifiedTicket.FromModelToPublishEntradaRequest();
+            Entrada verifiedTicket = await _gopassHttpClientService.GetTicketByQrAsync(publishReventaRequestDto.CodigoQR);
+            PublishEntradaRequestDto existingTicketInFaker = verifiedTicket.FromModelToPublishEntradaRequest();
 
-                Entrada createdTicket = await _entradaService.PublishTicket(existingTicketInFaker, userId);
+            Entrada createdTicket = await _entradaService.PublishTicket(existingTicketInFaker, userId);
 
-                Reventa reventaToPublish = publishReventaRequestDto.FromPublishReventaRequestToModel();
-                reventaToPublish.EntradaId = createdTicket.Id;
+            Reventa reventaToPublish = publishReventaRequestDto.FromPublishReventaRequestToModel();
+            reventaToPublish.EntradaId = createdTicket.Id;
 
-                Reventa publishedReventa = await _reventaService.PublishTicketAsync(reventaToPublish, userId);
+            Reventa publishedReventa = await _reventaService.PublishTicketAsync(reventaToPublish, userId);
 
-                return Ok(publishedReventa.FromModelToPublishReventaResponseDto());
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return Ok(publishedReventa.FromModelToPublishReventaResponseDto());
         }
 
         [Authorize]
