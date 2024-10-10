8bcf0454276e3ba706699f95eceb61b1638ee88b
Ezefeola <ezefeola@gmail.com>
2024-10-10 11:22:38 -0300

    se agrego el Id al SellerInformationResponseDto

--- GoPass.Domain/DTOs/Response/AuthResponseDTOs/SellerInformationResponseDto.cs
+++ GoPass.Domain/DTOs/Response/AuthResponseDTOs/SellerInformationResponseDto.cs
@@ -8,6 +8,7 @@ namespace GoPass.Domain.DTOs.Response.AuthResponseDTOs
 {
     public class SellerInformationResponseDto
     {
+        public int Id { get; set; }
         public string Nombre { get; set; } = default!;
         public string Image { get; set; } = default!;
     }
